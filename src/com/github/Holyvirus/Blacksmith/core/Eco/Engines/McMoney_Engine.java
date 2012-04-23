e1c8aebbb8945a8bc16d5cdd872f978fda395cee
Giant <giant@tpre.nl>
2012-04-23 02:48:35 +0200

    Implement Eco into BlackSmith

---
+++ src/com/github/Holyvirus/Blacksmith/core/Eco/Engines/McMoney_Engine.java
@@ -0,0 +1,121 @@
+package com.github.Holyvirus.Blacksmith.core.Eco.Engines;
+
+import com.github.Holyvirus.Blacksmith.BlackSmith;
+import com.github.Holyvirus.Blacksmith.core.Eco.iEco;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.PluginEnableEvent;
+import org.bukkit.event.server.PluginDisableEvent;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+import java.util.logging.Level;
+
+import boardinggamer.mcmoney.McMoneyAPI;
+
+/**
+ *
+ * @author Giant
+ */
+public class McMoney_Engine implements iEco {
+	
+	private BlackSmith plugin;
+	private McMoneyAPI eco = null;
+	
+	public McMoney_Engine(BlackSmith plugin) {
+		this.plugin = plugin;
+		Bukkit.getServer().getPluginManager().registerEvents(new EcoListener(this), plugin);
+		
+		if(eco == null) {
+			Plugin ecoEn = plugin.getServer().getPluginManager().getPlugin("McMoney");
+
+			if(ecoEn != null && ecoEn.isEnabled()) {
+				eco = McMoneyAPI.getInstance();
+				if(eco == null) {
+					plugin.getLogger().log(Level.WARNING, "Failed to hook into McMoney!");
+				}else{
+					plugin.getLogger().log(Level.INFO, "Succesfully hooked into McMoney!");
+				}
+			}
+		}
+	}
+	
+	@Override
+	public boolean isLoaded() {
+		return false;
+	}
+	
+	@Override
+	public double getBalance(Player player) {
+		return this.getBalance(player.getName());
+	}
+	
+	@Override
+	public double getBalance(String player) {
+		return eco.getMoney(player);
+	}
+	
+	@Override
+	public boolean withdraw(Player player, double amount) {
+		return this.withdraw(player.getName(), amount);
+	}
+	
+	@Override
+	public boolean withdraw(String player, double amount) {
+		if(amount > 0) {
+			if((eco.getMoney(player) - amount) > 0) {
+				eco.removeMoney(player, amount);
+				return true;
+			}
+		}
+					
+		return false;
+	}
+	
+	@Override
+	public boolean deposit(Player player, double amount) {
+		return this.deposit(player.getName(), amount);
+	}
+	
+	@Override
+	public boolean deposit(String player, double amount) {
+		if(amount > 0) {
+			eco.addMoney(player, amount);
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public class EcoListener implements Listener {
+		private McMoney_Engine eco;
+		
+		public EcoListener(McMoney_Engine eco) {
+			this.eco = eco;
+		}
+		
+		@EventHandler()
+		public void onPluginEnable(PluginEnableEvent event) {
+			if(eco.eco == null) {
+				Plugin ecoEn = plugin.getServer().getPluginManager().getPlugin("McMoney");
+				
+				if(ecoEn != null && ecoEn.isEnabled()) {
+					eco.eco = McMoneyAPI.getInstance();
+					plugin.getLogger().log(Level.INFO, "Succesfully hooked into McMoney!");
+				}
+			}
+		}
+		
+		@EventHandler()
+		public void onPluginDisable(PluginDisableEvent event) {
+			if(eco.eco != null) {
+				if(event.getPlugin().getDescription().getName().equals("McMoney")) {
+					eco.eco = null;
+					plugin.getLogger().log(Level.INFO, "Succesfully unhooked into McMoney!");
+				}
+			}
+		}
+	}
+}
