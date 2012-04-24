c4c71804c4181f0c7a8bae2383b5cc60006d4438
Giant <giant@tpre.nl>
2012-04-24 22:14:03 +0200

    Implement fix for SignChangeEvent being stupid

--- src/com/github/Holyvirus/Blacksmith/core/Tools/Sign/SignValidator.java
+++ src/com/github/Holyvirus/Blacksmith/core/Tools/Sign/SignValidator.java
@@ -1,24 +1,41 @@
 package com.github.Holyvirus.Blacksmith.core.Tools.Sign;
 
 import org.bukkit.block.Sign;
+import org.bukkit.event.block.SignChangeEvent;
 
 public class SignValidator {
 	
-	public static boolean isBlackSmithSign(Sign s) {
-		return s.getLine(0).equalsIgnoreCase("[BlackSmith]");
+	private static boolean isBlackSmithSign(String s) {
+		return s.equalsIgnoreCase("[BlackSmith]");
 	}
 	
-	public static SignType getType(Sign s) {
+	public static SignType getType(String s) {
 		if(isBlackSmithSign(s)) {
-			if(s.getLine(1).equalsIgnoreCase("VALUE")) {
+			if(s.equalsIgnoreCase("VALUE")) {
 				return SignType.VALUE;
-			}else if(s.getLine(1).equalsIgnoreCase("REPAIR")) {
+			}else if(s.equalsIgnoreCase("REPAIR")) {
 				return SignType.REPAIR;
-			}else if(s.getLine(1).equalsIgnoreCase("KILL")) {
+			}else if(s.equalsIgnoreCase("KILL")) {
 				return SignType.KILL;
 			}
 		}
 		
 		return SignType.INVALID;
+	}
+	
+	public static boolean isBlackSmithSign(Sign s) {
+		return isBlackSmithSign(s.getLine(0));
+	}
+	
+	public static boolean isBlackSmithSign(SignChangeEvent s) {
+		return isBlackSmithSign(s.getLine(0));
+	}
+	
+	public static SignType getType(Sign s) {
+		return getType(s.getLine(1));
+	}
+	
+	public static SignType getType(SignChangeEvent s) {
+		return getType(s.getLine(1));
 	}
 }
