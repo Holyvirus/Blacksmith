45840af74b15d9703fd9a27832538ab7b3f2be8d
Giant <giant@tpre.nl>
2012-04-24 18:39:42 +0200

    Continue work on PlayerListener

--- src/com/github/Holyvirus/Blacksmith/Listeners/PlayerListener.java
+++ src/com/github/Holyvirus/Blacksmith/Listeners/PlayerListener.java
@@ -2,10 +2,13 @@ package com.github.Holyvirus.Blacksmith.Listeners;
 
 import com.github.Holyvirus.Blacksmith.BlackSmith;
 //import com.github.Holyvirus.Blacksmith.core.Eco.iEco;
+import com.github.Holyvirus.Blacksmith.core.config;
 import com.github.Holyvirus.Blacksmith.core.perms.Permission;
+import com.github.Holyvirus.Blacksmith.core.Misc.Misc;
 import com.github.Holyvirus.Blacksmith.core.Tools.Sign.*;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
@@ -33,7 +36,12 @@ public class PlayerListener implements Listener {
 				switch(st) {
 					case VALUE:
 						if(pH.has(p, "blacksmith.use.value")) {
-							
+							Material m = Misc.getType(event.getItem());
+							if(m != null) {
+								
+							}else{
+								p.sendMessage(ChatColor.DARK_RED + "Item not a tool!");
+							}
 						}else{
 							p.sendMessage(ChatColor.DARK_RED + "You have no access to that!");
 						}
@@ -46,10 +54,17 @@ public class PlayerListener implements Listener {
 						}
 						break;
 					case KILL:
-						if(pH.has(p, "blacksmith.use.kill")) {
-							
+						config c = config.Obtain();
+						if(c.getBoolean("BlackSmith.global.debug")) {
+							if(pH.has(p, "blacksmith.use.kill")) {
+								
+							}else{
+								p.sendMessage(ChatColor.DARK_RED + "You have no access to that!");
+							}
 						}else{
-							p.sendMessage(ChatColor.DARK_RED + "You have no access to that!");
+							if(pH.has(p, "blacksmith.use.kill")) {
+								p.sendMessage(ChatColor.DARK_RED + "Debug mode is not enabled!");
+							}
 						}
 						break;
 					default:
