102650155b19549cfb33ba25dbede415abdf657b
Giant <giant@tpre.nl>
2012-04-26 00:23:22 +0200

    Make Repair compatible with material loader thing

--- src/com/github/Holyvirus/Blacksmith/core/Tools/Cost/Repair.java
+++ src/com/github/Holyvirus/Blacksmith/core/Tools/Cost/Repair.java
@@ -2,6 +2,7 @@ package com.github.Holyvirus.Blacksmith.core.Tools.Cost;
 
 import com.github.Holyvirus.Blacksmith.BlackSmith;
 import com.github.Holyvirus.Blacksmith.core.Eco.iEco;
+import com.github.Holyvirus.Blacksmith.core.Eco.mEco;
 
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -9,12 +10,15 @@ import org.bukkit.inventory.ItemStack;
 public class Repair {
 	
 	private static iEco eH;
+	private static mEco eM;
 	private static boolean FC = true;
 	
 	private static void init() {	
 		FC = false;
 		if(null != BlackSmith.getPlugin().getEcoHandler())
 			eH = BlackSmith.getPlugin().getEcoHandler().getEngine();
+		
+		eM = BlackSmith.getPlugin().getMatEngine();
 	}
 	
 	public static String doRepair(Player p, ItemStack i) {
