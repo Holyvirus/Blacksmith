a89995fc7b8a2dfa35e7b90e71735b2b746e1cc6
Giant <giant@tpre.nl>
2012-04-25 02:23:25 +0200

    God, I, Hate, Myself...

--- src/com/github/Holyvirus/Blacksmith/core/Misc/Misc.java
+++ src/com/github/Holyvirus/Blacksmith/core/Misc/Misc.java
@@ -5,6 +5,70 @@ import org.bukkit.Material;
 
 public class Misc {
 	
+	public static ToolType getKind(ItemStack i) {
+		if(i != null) {
+			switch(i.getTypeId()) {
+				case 298:
+				case 299:
+				case 300:
+				case 301:
+					return ToolType.LEATHER;
+				case 261:
+				case 268:
+				case 269:
+				case 270:
+				case 271:
+				case 290:
+				case 346:
+					return ToolType.WOOD;
+				case 272:
+				case 273:
+				case 274:
+				case 275:
+				case 291:
+					return ToolType.STONE;
+				case 256:
+				case 257:
+				case 258:
+				case 267:
+				case 292:
+				case 306:
+				case 307:
+				case 308:
+				case 309:
+					return ToolType.IRON;
+				case 276:
+				case 277:
+				case 278:
+				case 279:
+				case 293:
+				case 310:
+				case 311:
+				case 312:
+				case 313:
+					return ToolType.DIAMOND;
+				case 283:
+				case 284:
+				case 285:
+				case 286:
+				case 294:
+				case 314:
+				case 315:
+				case 316:
+				case 317:
+					return ToolType.GOLD;
+				case 302:
+				case 303:
+				case 304:
+				case 305:
+					return ToolType.CHAIN;
+			}
+				
+		}
+		
+		return ToolType.INVALID;
+	}
+	
 	public static Material getType(ItemStack i) {
 		if(i == null)
 			return null;
---
+++ src/com/github/Holyvirus/Blacksmith/core/Misc/ToolType.java
@@ -0,0 +1,13 @@
+package com.github.Holyvirus.Blacksmith.core.Misc;
+
+public enum ToolType {
+	LEATHER,
+	WOOD,
+	STONE,
+	IRON,
+	GOLD,
+	DIAMOND,
+	CHAIN,
+	
+	INVALID
+}
