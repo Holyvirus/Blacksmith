b1eb9d72529466b22eacee61e46ee373fb9d8e49
Giant <giant@tpre.nl>
2012-05-28 18:30:27 +0200

    Give names to tooltypes! :D

--- src/com/github/Holyvirus/Blacksmith/core/Misc/ToolType.java
+++ src/com/github/Holyvirus/Blacksmith/core/Misc/ToolType.java
@@ -1,27 +1,38 @@
 package com.github.Holyvirus.Blacksmith.core.Misc;
 
 public enum ToolType {
-	LEATHER,
-	WOOD,
-	STONE,
-	IRON,
-	GOLD,
-	DIAMOND,
-	CHAIN,
+	LEATHER("leather"),
+	WOOD("wood"),
+	STONE("stone"),
+	IRON("iron"),
+	GOLD("gold"),
+	DIAMOND("chain"),
+	CHAIN("chain"),
 	
-	SWORD,
-	SPADE,
-	AXE,
-	PICKAXE,
-	HOE,
-	HELMET,
-	CHEST,
-	LEG,
-	BOOT,
-	BOW,
-	ROD,
-	FLINT_AND_STEEL,
-	SHEARS,
+	SWORD("sword"),
+	SPADE("spade"),
+	AXE("axe"),
+	PICKAXE("pick"),
+	HOE("hoe"),
+	HELMET("helmet"),
+	CHEST("chest"),
+	LEG("pants"),
+	BOOT("boots"),
+	BOW("bow"),
+	ROD("fishingrod"),
+	FLINT_AND_STEEL("flint"),
+	SHEARS("shears"),
 	
-	INVALID
+	INVALID("invalid");
+	
+	private String s = "";
+	
+	private ToolType(String s) {
+		this.s = s;
+	}
+	
+	@Override
+	public String toString() {
+		return s;
+	}
 }
